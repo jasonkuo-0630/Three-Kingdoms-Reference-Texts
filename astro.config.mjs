5e2d279bbc38b632dfe214b9fb46abc3217ca0b1
郭宇軒 <jason910630@gmail.com>
2026-08-13 08:57:11 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://jasonkuo-0630.github.io",
   base: "/Three-Kingdoms-Reference-Texts",
